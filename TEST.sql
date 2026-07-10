2d27303b4661e24d545f332311b961c4dbfebd54
Humberto Bizarro Luvian <albertbilu29@gmail.com>
2026-07-10 15:49:47 -0600

    Add files via upload

---
+++ TEST.sql
@@ -0,0 +1,8 @@
+FIND PRODUCTS THAT CONTRIBUTE TO 80% OF THE REVENUE (Pareto analysis)
+
+1 i NEED TO GET THE PRODUCTID AND ITS REVENUE
+ CTE1
+2 I need to get the total REVENUE
+  I need to create a CTE2 from USING CTE1
+3 I need to get the cumulatiuve revenue
+   SUBQUERY WITH CTE1 AND cte2 cross join

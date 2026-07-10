6e1b0ad2941a066ac8dc568f941c072a698714a4
Humberto Bizarro Luvian <albertbilu29@gmail.com>
2026-07-10 15:50:09 -0600

    Delete TEST.sql

--- TEST.sql
+++
@@ -1,8 +0,0 @@
-FIND PRODUCTS THAT CONTRIBUTE TO 80% OF THE REVENUE (Pareto analysis)
-
-1 i NEED TO GET THE PRODUCTID AND ITS REVENUE
- CTE1
-2 I need to get the total REVENUE
-  I need to create a CTE2 from USING CTE1
-3 I need to get the cumulatiuve revenue
-   SUBQUERY WITH CTE1 AND cte2 cross join
